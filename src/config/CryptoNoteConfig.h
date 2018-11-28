053fbf0320f24f990950eb9a562ccf227b5822f8
andy <dirtybitsofficial@gmail.com>
2018-11-27 23:52:32 -0600

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -249,7 +249,7 @@ const static boost::uuids::uuid CRYPTONOTE_NETWORK =
 
 const char* const SEED_NODES[] =
 {
-	"67.176.94.193:11897",
+	"149.248.15.169:11897",
 	"45.77.96.235:11897"
 };
 } // CryptoNote
